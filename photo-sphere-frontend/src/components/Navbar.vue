bf818ad3c9896928a5b66838723306a2ac8225f8
syedokun <paradoxsyed@gmail.com>
2021-05-29 16:59:50 +0600

    Navbar Skeleton Added
    Navbar skeleton and the associated resources added to the project

---
+++ photo-sphere-frontend/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+     <div class="container">
+          <div class="logo">
+               <img class="icon" src="../../icons/logo-final.svg" alt="" />
+          </div>
+          <div class="navigation">
+               <div class="vertical">
+                    <a href="">Exhibition</a>
+               </div>
+               <div class="vertical">
+                    <a href="">MarketPlace</a>
+               </div>
+               <div class="vertical">
+                    <a href="">Profile/Login</a>
+               </div>
+          </div>
+     </div>
+</template>
+
+<script>
+export default {
+     name: 'Navbar',
+     props: {
+          logged_in: Boolean,
+          default: false,
+     },
+}
+</script>
+
+<style scoped>
+.container {
+     background-color: #6eb4ac80;
+     display: flex;
+     justify-content: space-between;
+     padding: 5px;
+}
+
+.icon {
+     width: 280px;
+}
+
+.navigation {
+     display: flex;
+     justify-content: space-between;
+}
+
+.vertical {
+     border-right: 1px solid rgba(0, 0, 0, 0.3);
+     height: 12px;
+     padding: 20px 15px;
+}
+
+a {
+     color: rgba(255, 255, 255, 0.95);
+     text-decoration: none;
+}
+</style>
---
+++ photo-sphere-frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+     <Navbar> </Navbar>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '../components/Navbar'
+
+export default {
+     name: 'Home',
+     components: {
+          Navbar,
+     },
+}
+</script>
