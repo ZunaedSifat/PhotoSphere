cbe48e74bc08d56efb8246d5f50daa325b053d04
zunaedsifat <zunaed.sifat@gmail.com>
2021-07-21 16:36:41 +0600

    added tags admin

--- backend/PhotoSphere/photos/admin.py
+++ backend/PhotoSphere/photos/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from photos.models import Photo
+from photos.models import Photo, Tag
 
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'uploader')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'created_at')
